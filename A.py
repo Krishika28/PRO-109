f3db519a7541dc76c240b3f6136c1357b31d7d4f
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2021-07-17 20:21:24 +0530

    Update A.py

--- A.py
+++ A.py
@@ -23,21 +23,23 @@ sd3_start, sd3_end = mean - (3*std), mean + (3*std)
 
 count_data1 = 0
 count_data2 = 0
+count_data3 = 0
+
 for i in list:
     if(i > sd1_start and i<sd1_end):
         count_data1 = count_data1 +1
     if(i > sd2_start and i<sd2_end):
         count_data2 = count_data2 +1 
-#   if(1 > sd3_start and i<sd3_end):
-#      count_data3 = count_data3 +1
+    if(1 > sd3_start and i<sd3_end):
+      count_data3 = count_data3 +1
 
 percentage_data1 = (count_data1/len(list)) *100
 percentage_data2 = (count_data2/len(list)) *100
-#percentage_data3 = (count_data3/len(list)) *100
+percentage_data3 = (count_data3/len(list)) *100
 
 print(percentage_data1, "of the data lies within 1st standard devation")
 print(percentage_data2, "of the data lies within 2nd standard devation")
-#print(percentage_data3, "of the data lies within 3rd standard devation")
+print(percentage_data3, "of the data lies within 3rd standard devation")
 
 fig = ff.create_distplot([list],["writing result"], show_hist=False)
 
